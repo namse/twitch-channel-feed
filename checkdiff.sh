9232a8ae36cf8b2840d1ec5d21f8d9d3c4e71352
skatpgusskat <skatpgusskat@naver.com>
2018-08-25 20:18:19 +0900

    Run travis if .travis.yml has been edited

--- checkdiff.sh
+++ checkdiff.sh
@@ -3,18 +3,18 @@
 set -e
 
 CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
-ONLY_READMES=True
+CONTINUE=False
 INCLUDE=$1
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  if ! [[ $CHANGED_FILE =~ $INCLUDE ]]; then
-    ONLY_READMES=False
+  if  [[ $CHANGED_FILE == ".travis.yml" ]] || [[ $CHANGED_FILE =~ $INCLUDE ]]; then
+    CONTINUE=True
     break
   fi
 done
 
-if [[ $ONLY_READMES == True ]]; then
-  echo "PASS"
+if [[ $CONTINUE == False ]]; then
+  echo "skip build"
   travis_terminate 0
   exit 1
 else
