c1c30701daedb297028e528d24e16d139aacbf25
skatpgusskat <skatpgusskat@naver.com>
2018-08-26 23:47:17 +0900

    Add prefix VUE_APP_

--- client/src/api/backendApi.ts
+++ client/src/api/backendApi.ts
@@ -4,7 +4,7 @@ const EMOTES_AVAILABLE_S3_BUCKET_ENDPOINT = 'https://s3.ap-northeast-2.amazonaws
 if (!process.env.API_GATEWAY_STAGE) {
   console.log(`api gateway stage not setup`);
 }
-const apiGatewayStage = process.env.API_GATEWAY_STAGE || 'develop';
+const apiGatewayStage = process.env.VUE_APP_API_GATEWAY_STAGE || 'develop';
 const BACKEND_ENDPOINT = `https://api.twitchchannelfeed.com/${apiGatewayStage}`;
 
 export async function check2xx(response: Response) {
