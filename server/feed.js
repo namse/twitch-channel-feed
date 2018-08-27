6051d8cfd5eb50ed7644e653a35b3ec6bd929219
skatpgusskat <skatpgusskat@naver.com>
2018-08-27 20:29:15 +0900

    Divide cold data

--- server/feed.js
+++ server/feed.js
@@ -1,8 +1,52 @@
 const AWS = require('aws-sdk');
 const authExtTokenHeader = require('./authExtTokenHeader');
+const uuid = require('uuid/v4');
 
 const s3 = new AWS.S3();
 const bucketName = 'twitch-channel-feed';
+const COLD_DATA_LENGTH = 3;
+
+function fetchOrNewRecent(recentKey) {
+  try {
+    const data = await s3.getObject({
+      Bucket: bucketName,
+      Key: recentKey,
+    }).promise();
+    return JSON.parse(data.Body);
+  } catch (err) {
+    if (err.code !== 'NoSuchKey') {
+      throw err;
+    }
+    return {
+      feeds: [],
+      nextData: null,
+    };
+  }
+}
+
+function divideColdData(recent) {
+  if (recnet.feeds.length < 2 * COLD_DATA_LENGTH) {
+    return {
+      newRecent: recent,
+      coldData: null,
+    };
+  }
+
+  const coldData = {
+    feeds: recent.slice(recent.feeds.length - COLD_DATA_LENGTH),
+    nextData: recent.nextData,
+    key: `${uuid()}.json`,
+  };
+  const newRecent = {
+    feeds: recent.feeds.slice(0, recent.feeds.length - COLD_DATA_LENGTH),
+    nextData: coldData.id,
+  };
+
+  return {
+    newRecent,
+    coldData,
+  };
+}
 
 module.exports.post = async (event, context, callback) => {
   try {
@@ -30,22 +74,7 @@ module.exports.post = async (event, context, callback) => {
     }
     const recentKey = `${userId}/recent.json`;
 
-    let recent
-    try {
-      const data = await s3.getObject({
-        Bucket: bucketName,
-        Key: recentKey,
-      }).promise();
-      recent = JSON.parse(data.Body);
-    } catch (err) {
-      if (err.code !== 'NoSuchKey') {
-        throw err;
-      }
-      recent = {
-        feeds: [],
-        nextData: null,
-      };
-    }
+    const recent = fetchOrNewRecent(recentKey);
 
     const {
       feeds,
@@ -55,10 +84,18 @@ module.exports.post = async (event, context, callback) => {
       content,
     });
 
+    const { newRecent, coldData } = divideColdData(recent);
+
     await s3.putObject({
       Bucket: bucketName,
       Key: recentKey,
-      Body: JSON.stringify(recent),
+      Body: JSON.stringify(newRecent),
+    }).promise();
+
+    await s3.putObject({
+      Bucket: bucketName,
+      Key: coldData.key,
+      Body: JSON.stringify(coldData),
     }).promise();
 
     const response = {
