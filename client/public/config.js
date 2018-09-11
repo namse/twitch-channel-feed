a5bba53cba85aea77711626870a230feb25a8356
skatpgusskat <skatpgusskat@naver.com>
2018-09-11 23:57:44 +0900

    Seperate config html and js

---
+++ client/public/config.js
@@ -0,0 +1,12 @@
+function openSync() {
+  var redirectUri = "https://sync-emote.twitchchannelfeed.com";
+  var responseType = "token+id_token";
+  var scope = ["openid", "user_subscriptions", "user_read"].join(" ");
+  var url = "https://id.twitch.tv/oauth2/authorize?client_id=04x1w8j9ieyvxl4c3j17173z0zh1e7&redirect_uri="
+    + redirectUri
+    + "&response_type="
+    + responseType
+    + "&scope="
+    + scope;
+  window.open(url);
+}
