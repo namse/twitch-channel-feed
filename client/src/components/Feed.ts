90670f85677b69d81e7de9f2aa798fdcf30d0a9a
skatpgusskat <skatpgusskat@naver.com>
2018-10-21 05:33:04 +0900

    Change time format

--- client/src/components/Feed.ts
+++ client/src/components/Feed.ts
@@ -3,12 +3,11 @@ import { Action } from 'vuex-class';
 import toJsonNodeFromString from '@/JsonNode/toJsonNodeFromString';
 import { Feed } from '../../../types/FeedFile';
 import ModalComponent from '@/components/Modal.vue';
-import moment from 'moment-timezone';
+import moment from 'moment';
 import { deletePost } from '@/api/backendApi';
 import { State } from 'vuex-class';
 import { ExtensionAuth } from '@/store/twitch';
 
-moment.tz.setDefault('Asia/Seoul');
 moment.locale('ko');
 
 @Component({
@@ -25,7 +24,8 @@ export default class FeedComponent extends Vue {
 
   htmlContent: string = '';
   isOpenDeleteModal: boolean = false;
-  readonly fromDateString: string = moment(this.feed.date).fromNow();
+
+  readonly fromDateString: string = moment(this.feed.date).format('LLL');
 
   @Watch('feed.content', { immediate: true })
   onFeedContentChanged(val: string, oldVal: string) {
