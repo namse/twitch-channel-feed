56f7925a0325986a5860956fcb604da4ecad5d85
skatpgusskat <skatpgusskat@naver.com>
2018-10-21 04:07:41 +0900

    Set moment timezone and locale as Korean

--- client/src/components/Feed.ts
+++ client/src/components/Feed.ts
@@ -3,10 +3,13 @@ import { Action } from 'vuex-class';
 import toJsonNodeFromString from '@/JsonNode/toJsonNodeFromString';
 import { Feed } from '../../../types/FeedFile';
 import ModalComponent from '@/components/Modal.vue';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import { deletePost } from '@/api/backendApi';
 import { State } from 'vuex-class';
 import { ExtensionAuth } from '@/store/twitch';
+
+moment.tz.setDefault('Asia/Seoul');
+moment.locale('ko');
 
 @Component({
   components: {
