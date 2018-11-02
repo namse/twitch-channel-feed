99959710e281e954ad4ee9090d3b74a4a2fc28d5
namse <namse@bluehole.net>
2018-11-02 23:39:09 +0900

    Encode image jpeg not webp

--- server/src/media/encodeImage.ts
+++ server/src/media/encodeImage.ts
@@ -1,32 +1,21 @@
 import sharp from 'sharp';
-import imagemin from 'imagemin';
-import imageminWebp from 'imagemin-webp';
 
 const MAX_WIDTH = 300;
 
-async function resizeImage(buffer) {
-  const image = sharp(buffer);
+export default async function encodeImage(buffer) {
+  let image = sharp(buffer);
 
   const { width } = await image.metadata();
 
   if (width <= MAX_WIDTH) {
-    return buffer;
+    image = image.resize(MAX_WIDTH);
   }
 
-  const resizedBuffer = await image.resize(MAX_WIDTH)
+  const jpegBuffer = await image
+    .jpeg({
+      progressive: true,
+    })
     .toBuffer();
 
-  return resizedBuffer;
-}
-
-export default async function encodeImage(buffer) {
-  const resizedBuffer = await resizeImage(buffer);
-
-  const resultBuffer = await imagemin.buffer(resizedBuffer, {
-    plugins: [
-      imageminWebp(),
-    ],
-  });
-
-  return resultBuffer;
+  return jpegBuffer;
 }
